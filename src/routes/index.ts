85012218e3a95b33a621c53f347902ba98d3622a
Joe Haddad <joe@sefactory.io>
2025-12-16 11:12:18 +0200

    Add email routes into main router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import healthRoutes from './health.routes';
+import emailRoutes from './email.routes';
 
 const router = Router();
 
 router.use('/health', healthRoutes);
+router.use('/', emailRoutes);
 
 export default router;
